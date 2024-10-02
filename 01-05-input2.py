7994a8fcca0f6d6e98d088dfeb17a2c7453a754c
Helen Haveloh <h.havelohh@gmail.com>
2024-10-02 10:59:28 +0200

    added second input example

---
+++ 01-05-input2.py
@@ -0,0 +1,13 @@
+# wir definieren eine Variable mit dem Namen variable
+# verwende die input-Funktion, um den User aufzufordern, einen Wert einzugeben
+# der Wert der eingegeben wurde, wird dann in der Variablen variable gespeichert
+variable = input("Bitte gib den Wert für die Variable ein: ")
+
+# gebe den Wert der Variablen variable aus
+print("Der Wert der Variablen ist:", variable)
+
+# zweites Beispiel
+name = input("Wie heißt du? ")
+print("Hallo, " + name + "!")
+print(f"Hallo, {name}!")
+print("Hallo", name)
--- 01-06-functions.py
+++ 01-06-functions.py
@@ -2,11 +2,15 @@ def begruessung(name):
     print(f"Hallo, {name}!")
     
 begruessung("Alice")  # Ausgabe: Hallo, Alice!
+
+
 def addiere(x, y):
     return x + y
 
 ergebnis = addiere(10, 20)
 print(ergebnis)  # Ausgabe: 30
+
+
 def beschreibe_person(name, alter=30):
     print(f"{name} ist {alter} Jahre alt.")
     
